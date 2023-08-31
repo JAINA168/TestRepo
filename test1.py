af773546b99ab1e9e8d3879080a5022834c24115
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-08-31 12:31:42 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,10 @@
-test1.py
+            steps{		
+		        sh 'chmod +x devops_scripts/autosys_deploy.sh' 
+		        withCredentials([usernamePassword(credentialsId: 'sfaops', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        		    script {
+            			env.PASSWORD = sh(script: "echo \$PASSWORD", returnStdout: true).trim()
+            			env.USERNAME = sh(script: "echo \$USERNAME", returnStdout: true).trim()
+        		    } 	
+			    sh 'devops_scripts/autosys_deploy.sh'			
+		        }
+            }	
