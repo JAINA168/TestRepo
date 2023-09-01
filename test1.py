332fb6bbef337e96c6aecd2efe20ee1f5cf3d275
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-01 14:23:25 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,22 +1,41 @@
-tarted by user srvamr-sfaops
-org.jenkinsci.plugins.github_branch_source.RateLimitExceededException: GitHub API rate limit exceeded
-	at org.jenkinsci.plugins.github_branch_source.Connector$1.onError(Connector.java:586)
-	at org.kohsuke.github.RateLimitHandler.onError(RateLimitHandler.java:43)
-	at org.kohsuke.github.GitHubClient.detectKnownErrors(GitHubClient.java:417)
-	at org.kohsuke.github.GitHubClient.sendRequest(GitHubClient.java:386)
-Caused: org.kohsuke.github.HttpException: {"message":"API rate limit exceeded for 168.224.160.14. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)","documentation_url":"https://docs.github.com/rest/overview/resources-in-the-rest-api#rate-limiting"}
-	at org.kohsuke.github.GitHubClient.interpretApiError(GitHubClient.java:543)
-	at org.kohsuke.github.GitHubClient.sendRequest(GitHubClient.java:401)
-	at org.kohsuke.github.GitHubClient.fetch(GitHubClient.java:121)
-	at org.kohsuke.github.GitHubClient.checkApiUrlValidity(GitHubClient.java:323)
-	at org.kohsuke.github.GitHub.checkApiUrlValidity(GitHub.java:1244)
-	at org.jenkinsci.plugins.github_branch_source.ApiRateLimitChecker.verifyConnection(ApiRateLimitChecker.java:199)
-	at org.jenkinsci.plugins.github_branch_source.Connector$GitHubConnection.verifyConnection(Connector.java:794)
-	at org.jenkinsci.plugins.github_branch_source.Connector.connect(Connector.java:447)
-	at org.jenkinsci.plugins.github_branch_source.GitHubSCMSource.retrieve(GitHubSCMSource.java:1679)
-	at jenkins.scm.api.SCMSource.fetch(SCMSource.java:582)
-	at org.jenkinsci.plugins.workflow.multibranch.SCMBinder.create(SCMBinder.java:101)
-	at org.jenkinsci.plugins.workflow.job.WorkflowRun.run(WorkflowRun.java:311)
-	at hudson.model.ResourceController.execute(ResourceController.java:107)
-	at hudson.model.Executor.run(Executor.java:449)
-Finished: FAILURE
+stage ("Deploy to Unix"){
+            when {
+                 expression { params.Deploy_to_Unix == "Yes" }
+            }
+                steps{
+                    script{
+                        unix_deploy(src: unix_src_path_scripts, dest: unix_deploy_path_scripts, server: unix_server, service_account: unix_service_account, permissions: unix_permission)
+                        unix_deploy(src: unix_src_path_params, dest: unix_deploy_path_params, server: unix_server, service_account: unix_service_account, permissions: unix_permission)
+                        unix_deploy(src: unix_src_path_scripts_ui, dest: unix_deploy_path_scripts_ui, server: unix_server, service_account: unix_service_account, permissions: unix_permission)
+                        }
+                }
+        }
+
+
+#!/usr/bin/env groovy
+
+def call(Map config) {
+    println "Deploying code into Unix environment"
+    // withCredentials([usernamePassword(credentialsId: "${var1}", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    //     println USER
+    //     println PASS
+    // }
+    println config.src
+    println config.dest
+    println config.server
+    println config.service_account
+    println config.permissions
+    //println config.group
+    //println config.owner
+    sh "ls"
+    if(config.service_account == 'srvamr-palign@amer'){
+        priv_key_path = '/var/lib/jenkins/.ssh/palign_id_rsa'
+    }else{
+        priv_key_path = '/var/lib/jenkins/.ssh/id_rsa'
+    }
+    sh "scp -i ${priv_key_path} -r ${config.src}/* ${config.service_account}@${config.server}:${config.dest}"
+    // Set Permissions of the destination files
+    sh "ssh -i ${priv_key_path} ${config.service_account}@${config.server} 'sudo chmod ${config.permissions} ${config.dest}/*'"
+    //Set the owner and file permissions inside a folder
+    // sh "ssh ${config.service_account}@${config.server} 'chown ${config.owner}:${config.group} ${config.dest}/*'"
+}
