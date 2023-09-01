0cef83161cd1099bfdb8d08958e20465fd0d6566
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-01 09:05:45 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,7 @@
-            steps{		
-		        sh 'chmod +x devops_scripts/autosys_deploy.sh' 
-		        withCredentials([usernamePassword(credentialsId: 'sfaops', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        		    script {
-            			env.PASSWORD = sh(script: "echo \$PASSWORD", returnStdout: true).trim()
-            			env.USERNAME = sh(script: "echo \$USERNAME", returnStdout: true).trim()
-        		    } 	
-			    sh 'devops_scripts/autosys_deploy.sh'			
-		        }
-            }	
+WorkflowScript: 25: Environment variable values must either be single quoted, double quoted, or function calls. @ line 25, column 27.
+                   dry_run = params.dry_run
+                             ^
+
+WorkflowScript: 24: No variables specified for environment @ line 24, column 13.
+               environment {
+               ^
