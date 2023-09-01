dd512673114d4776fdcea49fd2e3cda6167b3241
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-01 12:40:32 +0530

    Update autosys_deploy.sh

--- devops_scripts/autosys_deploy.sh
+++ devops_scripts/autosys_deploy.sh
@@ -5,25 +5,23 @@ successfulFiles=() # Array to store successful JIL files
 
 # Access the dry_run parameter as the first command-line argument ($1)
 dry_run="${dry_run}"
-echo "Dry run is  is : ${dry_run}"
+echo "Dry run is: ${dry_run}"
 USERNAME="${USERNAME}"
 PASSWORD="${PASSWORD}"
 
-
 target_branch=${GIT_BRANCH}
-echo "target branch is : ${target_branch}"
+echo "Target branch is: ${target_branch}"
 
 # Get a list of JIL files in the directory
-jilFiles=$(find ${jilDirectory} -name '*.jil')
+jilFiles=$(find "${jilDirectory}" -name '*.jil')
 
 # Iterate over the JIL files and make POST requests
 for jilFile in ${jilFiles}; do
     echo "Processing file: ${jilFile}"
-    if [ "${dry_run}" = Yes ]; then
+    if [ "${dry_run}" = "Yes" ]; then
         echo "Dry run completed. No changes were made."
-        #exit 0 # Exit with a successful status since this was just a dry run
     else
-		if [ "${target_branch}" = "test" ]; then
+        if [ "${target_branch}" = "test" ]; then
 			# Replace string in the JIL file for the "test" branch
 			sed -i 's/d2compaus/t2compaus/g' "${jilFile}"
 			sed -i 's/D2COMPAUS/T2COMPAUS/g' "${jilFile}"
@@ -39,8 +37,7 @@ for jilFile in ${jilFiles}; do
 			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN TEST/g' "${jilFile}"
 			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN TEST/g' "${jilFile}"
 			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_test/g' "${jilFile}"
-			
-		elif [ "${target_branch}" = "sit" ]; then
+        elif [ "${target_branch}" = "sit" ]; then
 			# Replace string in the JIL file for the "sit" branch
 			sed -i 's/d2compaus/s2compaus/g' "${jilFile}"
 			sed -i 's/D2COMPAUS/S2COMPAUS/g' "${jilFile}"
@@ -56,8 +53,7 @@ for jilFile in ${jilFiles}; do
 			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN SIT/g' "${jilFile}"
 			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN SIT/g' "${jilFile}"
 			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods/g' "${jilFile}"
-		
-		elif [ "${target_branch}" = "uat" ]; then
+        elif [ "${target_branch}" = "uat" ]; then
 			# Replace string in the JIL file for the "uat" branch
 			sed -i 's/d2compaus/u2compaus/g' "${jilFile}"
 			sed -i 's/D2COMPAUS/U2COMPAUS/g' "${jilFile}" 
@@ -73,9 +69,7 @@ for jilFile in ${jilFiles}; do
 			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN UAT/g' "${jilFile}"
 			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN UAT/g' "${jilFile}"
 			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_stg/g' "${jilFile}"
-			
-		
-		elif [ "${target_branch}" = "main" ]; then
+        elif [ "${target_branch}" = "main" ]; then
 			# Replace string in the JIL file for the "main" branch
 			sed -i 's/d2compaus/p2compaus/g' "${jilFile}"
 			sed -i 's/D2COMPAUS/P2COMPAUS/g' "${jilFile}"
@@ -91,29 +85,30 @@ for jilFile in ${jilFiles}; do
 			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN PROD/g' "${jilFile}"
 			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN PROD/g' "${jilFile}"
 			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_prod/g' "${jilFile}"
-		
-		# Perform the curl command
-		response=$(curl -X POST -H 'Content-Type: text/plain' --upload-file "${jilFile}" ${autosys_apiEndpoint} -k --user "${USERNAME}:${PASSWORD}" -i)
-		
-		# Print the response
-		echo "Response: ${response}"
-		
-		# Extract the value of the "status" field from the JSON response
-		statusMatch=$(echo "${response}" | grep -o '"status"\s*:\s*"[^"]*"' | awk -F'"' '{print $4}')
-		
-		if [ -n "${statusMatch}" ]; then
-			if [ "${statusMatch}" = "failed" ]; then
-				echo "Deployment of ${jilFile} failed"
-				failedFiles+=("${jilFile}") # Add failed JIL file to the array
-			else
-				echo "Deployment of ${jilFile} successful"
-				successfulFiles+=("${jilFile}") # Add successful JIL file to the array
-			fi
-		else
-			echo "Unable to determine the status from the response"
-			failedFiles+=("${jilFile}") # Assume failure if status extraction fails
-		fi
-	fi	
+        fi
+
+        # Perform the curl command
+        response=$(curl -X POST -H 'Content-Type: text/plain' --upload-file "${jilFile}" "${autosys_apiEndpoint}" -k --user "${USERNAME}:${PASSWORD}" -i)
+    
+        # Print the response
+        echo "Response: ${response}"
+    
+        # Extract the value of the "status" field from the JSON response
+        statusMatch=$(echo "${response}" | grep -o '"status"\s*:\s*"[^"]*"' | awk -F'"' '{print $4}')
+    
+        if [ -n "${statusMatch}" ]; then
+            if [ "${statusMatch}" = "failed" ]; then
+                echo "Deployment of ${jilFile} failed"
+                failedFiles+=("${jilFile}") # Add failed JIL file to the array
+            else
+                echo "Deployment of ${jilFile} successful"
+                successfulFiles+=("${jilFile}") # Add successful JIL file to the array
+            fi
+        else
+            echo "Unable to determine the status from the response"
+            failedFiles+=("${jilFile}") # Assume failure if status extraction fails
+        fi
+    fi
 done
 
 if [ ${#failedFiles[@]} -gt 0 ]; then
