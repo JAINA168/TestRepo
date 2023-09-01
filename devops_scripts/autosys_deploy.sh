0ed7c36d896835ee4a222e31fa836943973837eb
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-01 12:32:33 +0530

    Update autosys_deploy.sh

--- devops_scripts/autosys_deploy.sh
+++ devops_scripts/autosys_deploy.sh
@@ -22,98 +22,98 @@ for jilFile in ${jilFiles}; do
     if [ "${dry_run}" = Yes ]; then
         echo "Dry run completed. No changes were made."
         #exit 0 # Exit with a successful status since this was just a dry run
-    fi
-    if [ "${target_branch}" = "test" ]; then
-        # Replace string in the JIL file for the "test" branch
-        sed -i 's/d2compaus/t2compaus/g' "${jilFile}"
-        sed -i 's/D2COMPAUS/T2COMPAUS/g' "${jilFile}"
-        sed -i "s/$autosys_dev_server/$autosys_test_server/g" "${jilFile}"
-        sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN TEST \1 PFZALGN_TEST PFZALGN_TEST/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN TEST/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN TEST_IDL/g' "${jilFile}"
-        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_test/g' "${jilFile}"
-        sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods_test/g' "${jilFile}"
-        #sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh TEST/g' "${jilFile}"
-        sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh TEST/g' "${jilFile}"
-        sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD TEST_IDL/g' "${jilFile}"
-        sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN TEST/g' "${jilFile}"
-        sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN TEST/g' "${jilFile}"
-        sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_test/g' "${jilFile}"
-        
-    fi
-    if [ "${target_branch}" = "sit" ]; then
-        # Replace string in the JIL file for the "sit" branch
-        sed -i 's/d2compaus/s2compaus/g' "${jilFile}"
-        sed -i 's/D2COMPAUS/S2COMPAUS/g' "${jilFile}"
-        sed -i "s/$autosys_dev_server/$autosys_sit_server/g" "${jilFile}"
-        sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN SIT \1 PFZALGN PFZALGN/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN SIT/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN SIT_IDL/g' "${jilFile}"
-        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods/g' "${jilFile}"
-        sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods/g' "${jilFile}"
-        #sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh SIT/g' "${jilFile}"
-        sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh SIT/g' "${jilFile}"
-        sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD SIT_IDL/g' "${jilFile}"
-        sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN SIT/g' "${jilFile}"
-        sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN SIT/g' "${jilFile}"
-        sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods/g' "${jilFile}"
-    fi
-    if [ "${target_branch}" = "uat" ]; then
-        # Replace string in the JIL file for the "uat" branch
-        sed -i 's/d2compaus/u2compaus/g' "${jilFile}"
-        sed -i 's/D2COMPAUS/U2COMPAUS/g' "${jilFile}" 
-        sed -i "s/$autosys_dev_server/$autosys_uat_server/g" "${jilFile}"
-        sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN UAT \1 PFZALGN_STG PFZALGN_STG/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN UAT/g' "${jilFile}"
-        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_stg/g' "${jilFile}"
-        sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods_stg/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN UAT_IDL/g' "${jilFile}"
-        #sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh UAT/g' "${jilFile}"
-        sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh UAT/g' "${jilFile}"
-        sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD UAT_IDL/g' "${jilFile}"
-        sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN UAT/g' "${jilFile}"
-        sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN UAT/g' "${jilFile}"
-        sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_stg/g' "${jilFile}"
-        
-    fi
-    if [ "${target_branch}" = "main" ]; then
-        # Replace string in the JIL file for the "main" branch
-        sed -i 's/d2compaus/p2compaus/g' "${jilFile}"
-        sed -i 's/D2COMPAUS/P2COMPAUS/g' "${jilFile}"
-        sed -i "s/$autosys_dev_server/$autosys_prod_server/g" "${jilFile}"
-        sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN PROD \1 PFZALGN PFZALGN/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN PROD/g' "${jilFile}"
-        sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_prod/g' "${jilFile}"
-        sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods_prod/g' "${jilFile}"
-        sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN PROD_IDL/g' "${jilFile}"
-        #sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh PROD/g' "${jilFile}"
-        sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh PROD/g' "${jilFile}"
-        sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD PROD_IDL/g' "${jilFile}"
-        sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN PROD/g' "${jilFile}"
-        sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN PROD/g' "${jilFile}"
-        sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_prod/g' "${jilFile}"
-    fi
-    # Perform the curl command
-    response=$(curl -X POST -H 'Content-Type: text/plain' --upload-file "${jilFile}" ${autosys_apiEndpoint} -k --user "${USERNAME}:${PASSWORD}" -i)
-    
-    # Print the response
-    echo "Response: ${response}"
-    
-    # Extract the value of the "status" field from the JSON response
-    statusMatch=$(echo "${response}" | grep -o '"status"\s*:\s*"[^"]*"' | awk -F'"' '{print $4}')
-    
-    if [ -n "${statusMatch}" ]; then
-        if [ "${statusMatch}" = "failed" ]; then
-            echo "Deployment of ${jilFile} failed"
-            failedFiles+=("${jilFile}") # Add failed JIL file to the array
-        else
-            echo "Deployment of ${jilFile} successful"
-            successfulFiles+=("${jilFile}") # Add successful JIL file to the array
-        fi
     else
-        echo "Unable to determine the status from the response"
-        failedFiles+=("${jilFile}") # Assume failure if status extraction fails
-    fi
+		if [ "${target_branch}" = "test" ]; then
+			# Replace string in the JIL file for the "test" branch
+			sed -i 's/d2compaus/t2compaus/g' "${jilFile}"
+			sed -i 's/D2COMPAUS/T2COMPAUS/g' "${jilFile}"
+			sed -i "s/$autosys_dev_server/$autosys_test_server/g" "${jilFile}"
+			sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN TEST \1 PFZALGN_TEST PFZALGN_TEST/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN TEST/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN TEST_IDL/g' "${jilFile}"
+			sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_test/g' "${jilFile}"
+			sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods_test/g' "${jilFile}"
+			#sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh TEST/g' "${jilFile}"
+			sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh TEST/g' "${jilFile}"
+			sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD TEST_IDL/g' "${jilFile}"
+			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN TEST/g' "${jilFile}"
+			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN TEST/g' "${jilFile}"
+			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_test/g' "${jilFile}"
+			
+		elif [ "${target_branch}" = "sit" ]; then
+			# Replace string in the JIL file for the "sit" branch
+			sed -i 's/d2compaus/s2compaus/g' "${jilFile}"
+			sed -i 's/D2COMPAUS/S2COMPAUS/g' "${jilFile}"
+			sed -i "s/$autosys_dev_server/$autosys_sit_server/g" "${jilFile}"
+			sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN SIT \1 PFZALGN PFZALGN/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN SIT/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN SIT_IDL/g' "${jilFile}"
+			sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods/g' "${jilFile}"
+			sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods/g' "${jilFile}"
+			#sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh SIT/g' "${jilFile}"
+			sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh SIT/g' "${jilFile}"
+			sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD SIT_IDL/g' "${jilFile}"
+			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN SIT/g' "${jilFile}"
+			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN SIT/g' "${jilFile}"
+			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods/g' "${jilFile}"
+		
+		elif [ "${target_branch}" = "uat" ]; then
+			# Replace string in the JIL file for the "uat" branch
+			sed -i 's/d2compaus/u2compaus/g' "${jilFile}"
+			sed -i 's/D2COMPAUS/U2COMPAUS/g' "${jilFile}" 
+			sed -i "s/$autosys_dev_server/$autosys_uat_server/g" "${jilFile}"
+			sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN UAT \1 PFZALGN_STG PFZALGN_STG/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN UAT/g' "${jilFile}"
+			sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_stg/g' "${jilFile}"
+			sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods_stg/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN UAT_IDL/g' "${jilFile}"
+			#sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh UAT/g' "${jilFile}"
+			sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh UAT/g' "${jilFile}"
+			sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD UAT_IDL/g' "${jilFile}"
+			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN UAT/g' "${jilFile}"
+			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN UAT/g' "${jilFile}"
+			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_stg/g' "${jilFile}"
+			
+		
+		elif [ "${target_branch}" = "main" ]; then
+			# Replace string in the JIL file for the "main" branch
+			sed -i 's/d2compaus/p2compaus/g' "${jilFile}"
+			sed -i 's/D2COMPAUS/P2COMPAUS/g' "${jilFile}"
+			sed -i "s/$autosys_dev_server/$autosys_prod_server/g" "${jilFile}"
+			sed -i 's/pa_matillion_master\.ksh PALIGN DEV \(.*\) PFZALGN_DEV PFZALGN_DEV/pa_matillion_master.ksh PALIGN PROD \1 PFZALGN PFZALGN/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV/pa_postgresql_master.ksh PALIGN PROD/g' "${jilFile}"
+			sed -i 's/pa_snowflake_master.ksh cometl_pa_ods_dev/pa_snowflake_master.ksh cometl_pa_ods_prod/g' "${jilFile}"
+			sed -i 's/pa_sf_ae_load_status_check.ksh cometl_pa_ods_dev/pa_sf_ae_load_status_check.ksh cometl_pa_ods_prod/g' "${jilFile}"
+			sed -i 's/pa_postgresql_master.ksh PALIGN DEV_IDL/pa_postgresql_master.ksh PALIGN PROD_IDL/g' "${jilFile}"
+			#sed -i 's/master_batch_upload.ksh DEV/master_batch_upload.ksh PROD/g' "${jilFile}"
+			sed -i 's/master_cascade_notification.ksh DEV/master_cascade_notification.ksh PROD/g' "${jilFile}"
+			sed -i 's/master_dataloader.ksh ORACLE PROD DEV_IDL/master_dataloader.ksh ORACLE PROD PROD_IDL/g' "${jilFile}"
+			sed -i 's/pa_file_transfer.ksh PALIGN DEV/pa_file_transfer.ksh PALIGN PROD/g' "${jilFile}"
+			sed -i 's/pa_file_purge.ksh PALIGN DEV/pa_file_purge.ksh PALIGN PROD/g' "${jilFile}"
+			sed -i 's/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_dev/pa_snowflake_prevalidn_master.ksh cometl_pa_ods_prod/g' "${jilFile}"
+		
+		# Perform the curl command
+		response=$(curl -X POST -H 'Content-Type: text/plain' --upload-file "${jilFile}" ${autosys_apiEndpoint} -k --user "${USERNAME}:${PASSWORD}" -i)
+		
+		# Print the response
+		echo "Response: ${response}"
+		
+		# Extract the value of the "status" field from the JSON response
+		statusMatch=$(echo "${response}" | grep -o '"status"\s*:\s*"[^"]*"' | awk -F'"' '{print $4}')
+		
+		if [ -n "${statusMatch}" ]; then
+			if [ "${statusMatch}" = "failed" ]; then
+				echo "Deployment of ${jilFile} failed"
+				failedFiles+=("${jilFile}") # Add failed JIL file to the array
+			else
+				echo "Deployment of ${jilFile} successful"
+				successfulFiles+=("${jilFile}") # Add successful JIL file to the array
+			fi
+		else
+			echo "Unable to determine the status from the response"
+			failedFiles+=("${jilFile}") # Assume failure if status extraction fails
+		fi
+	fi	
 done
 
 if [ ${#failedFiles[@]} -gt 0 ]; then
