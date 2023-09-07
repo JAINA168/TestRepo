03dcec6d90698a36e0502c7a1d7264110af523c3
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-07 15:31:57 +0530

    Update autosys_deploy.sh

--- devops_scripts/autosys_deploy.sh
+++ devops_scripts/autosys_deploy.sh
@@ -4,7 +4,7 @@ failedFiles=() # Array to store failed JIL files
 successfulFiles=() # Array to store successful JIL files
 
 #dry_run= "${params.dry_run}"
-echo "Dry run is: ${params.dry_run}"
+echo "Dry run is: "${params.dry_run}""
 USERNAME="${USERNAME}"
 PASSWORD="${PASSWORD}"
 
