f026a995beebda9cf71e9b63a52d763d4d01b937
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-14 13:38:24 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,2 @@
-            Job Parameters:
-            Deploy_to_PostgreSQL: ${params.Deploy_to_PostgreSQL}
-            Deploy_to_Unix: ${params.Deploy_to_Unix}
-            Deploy_to_Autosys: ${params.Deploy_to_Autosys}
-            dry_run: ${params.dry_run}
-            Email_Alert: ${params.Email_Alert}
-            Notify_to: ${params.Notify_to}
+def projectNameSegments = currentBuild.fullProjectName.split('/')
+def technology = projectNameSegments[-1]
