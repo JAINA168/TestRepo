b832b49b9e3ca4b5066c0dde8992da29a5e6c0c7
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-19 19:10:31 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
-def initiator = currentBuild.causes[0]?.userName ?: "Unknown"
+<html>
+  <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
+
+    <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+      <h2 style="color: #007bff;">Your Heading</h2>
+      <p>Your content goes here...</p>
+      <a href="https://example.com" style="background-color: #007bff; color: #fff; padding: 10px 20px; border-radius: 5px; text-decoration: none;">Click Me</a>
+    </div>
+
+  </body>
+</html>
