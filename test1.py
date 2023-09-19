40a971d26550fb1529d9501d90e8960a242c6220
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-19 14:38:33 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,44 +1,42 @@
-<html>
+            <html>
             <body>
-                <h2>The Jenkins Deployment Job Sandbox/Replication-test/main is SUCCESS</h2>
+                <h2>The Jenkins Deployment Job $env.JOB_NAME is $currentBuild.result</h2>
                 
                 <hr/>
                 
                 <h3>Build Information:</h3>
                 <ul>
-                    <li>Build URL: <a href='http://10.90.82.223:8080/job/Sandbox/job/Replication-test/job/main/108/'>http://10.90.82.223:8080/job/Sandbox/job/Replication-test/job/main/108/</a></li>
-                    <li>Build Result: SUCCESS</li>
-                    <li>Github URL: <a href='https://github.com/HemapriyaM23/TestRepo.git'>https://github.com/HemapriyaM23/TestRepo.git</a></li>
-                    <li>Github Branch: main</li>
-                    <li>Deployment Request Id: DR2023091904217e</li>
+                    <!-- Build Information List -->
                 </ul>
                 
                 <hr/>
-                               
-                <h3>Pod Name: null</h3>
+                
                 <h3>Job Parameters:</h3>
-                <ul>
-                    <li>Deploy_to_PostgreSQL: null</li>
-                    <li>Deploy_to_Unix: No</li>
-                    <li>Deploy_to_Autosys: Yes</li>
-                    <li>dry_run: Yes</li>
-                    <li>Email_Alert: Yes</li>
-                    <li>Notify_to: hemapriya.m@pfizer.com</li>
-                </ul>
+                <table border="1">
+                    <tr>
+                        <th>Parameter Name</th>
+                        <th>Value</th>
+                    </tr>
+                    <tr>
+                        <td>Deploy_to_PostgreSQL</td>
+                        <td>${params.Deploy_to_PostgreSQL}</td>
+                    </tr>
+                    <tr>
+                        <td>Deploy_to_Unix</td>
+                        <td>${params.Deploy_to_Unix}</td>
+                    </tr>
+                    <tr>
+                        <td>Deploy_to_Autosys</td>
+                        <td>${params.Deploy_to_Autosys}</td>
+                    </tr>
+                    <!-- Add more parameters as needed -->
+                </table>
                 
                 <hr/>
                 
-
+                <!-- Add the rest of your content here -->
                 
-                <hr/>
-                
-                <p>
-                    For further queries, please reach out to <a href='mailto:DL-SFADevOps@pfizer.com'>DL-SFADevOps@pfizer.com</a>.
-                </p>
-                
-                <p>
-                    Thanks,<br/>
-                    SFA DevOps Team
-                </p>
             </body>
             </html>
+        """
+        
