5dd4f064e1f0f9f8145eec335083c1f3271177c9
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-20 19:00:35 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,85 +1,13 @@
-#!/usr/bin/env groovy
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.UUID
-
-def call(Map config){
-    if ((env.BRANCH_NAME != 'dev') || (config.Email_Alert == 'Yes')){
-        sub = " $currentBuild.result - $currentBuild.fullProjectName Jenkins Job"
-        //recp = config.Notify_to
-        def requestId = generateRequestID()
-        def projectNameSegments = currentBuild.fullProjectName.split('/')
-        def pod_name = projectNameSegments[-2]
-        def initiator = env.USER 
-        recp = 'cc:$DEFAULT_RECIPIENTS,' + config.Notify_to
-        msg = """
-            <html>
-            <body style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px;">
-              <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-                <h2 style="color: #007bff;">The Jenkins Deployment Job $env.JOB_NAME is $currentBuild.result</h2>
-                
-                <hr/>
-                
-                <h3>Build Information:</h3>
-                <table border="1" style="background-color: #ffffff; border-collapse: collapse; width: 100%;">
-                    <!-- ... (previous rows) ... -->
-                </table>
-                
-                <hr/>
-
-                <h3 style="background-color: #ffffff;">Pod Name: $pod_name</h3>
-                <h3>Job Parameters:</h3>
-                <table border="1" style="background-color: #ffffff; border-collapse: collapse; width: 100%;">
-                    <!-- ... (previous rows) ... -->
-                </table>
-                
-                <hr/>
-                
-                <hr/>
-                <p>
-                    For further queries, please reach out to <a href='mailto:DL-SFADevOps@pfizer.com'>DL-SFADevOps@pfizer.com</a>.
-                </p>
-                
-                <p>
-                    Thanks,<br/>
-                    SFA DevOps Team
-                </p>
-              </div>  
-            </body>
-            </html>
-        """
-        
-        emailext attachLog: true, body: "$msg", 
-                recipientProviders: [requestor()], 
-                replyTo: 'DL-SFADevOps@pfizer.com', 
-                subject: "$sub",
-                to: "$recp",
-                contentType: 'text/HTML'
-        println """
-            Build Number: $env.BUILD_NUMBER
-            Branch Name: $env.BRANCH_NAME
-            JOB URL: $env.JOB_DISPLAY_URL
-            BUILD_NUMBER: $env.BUILD_NUMBER
-            Build ID: $env.BUILD_ID
-            JOB_NAME: $env.JOB_NAME
-            Build_Status: $currentBuild.result
-            Project Name: $currentBuild.fullProjectName
-            """
-
-    }
-}
-
-// Generate unique request ID with prefix "DR"
-def generateRequestID() {
-    def prefix = "DR"
-    def timestamp = new Date().time
-    def uniqueID = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 2) // Truncate to 2 characters
-    def requestID = "${prefix}${formatTimestamp(timestamp)}${uniqueID}"
-    return requestID
-}
-
-// Format timestamp as yyyyMMddHHmmss
-def formatTimestamp(timestamp) {
-    def format = new SimpleDateFormat("yyyyMMddHHmm")
-    return format.format(new Date(timestamp))
-}
+Started by user srvamr-sfaops
+09:20:48 Connecting to https://api.github.com using SRVAMR-COMMGIT/****** (SRVAMR-COMMGIT)
+Obtained Jenkinsfile from 2956f52cf95fdddc625519d94e192e5b52f02d86
+Loading library sfdi-devops-tools-infra@feature/FIE-228
+Examining pfizer/sfdi-devops-tools-infra
+Attempting to resolve feature/FIE-228 as a branch
+Resolved feature/FIE-228 as branch feature/FIE-228 at revision 90158784d292f795fa7bb713c213ecd1b7797bd8
+Selected Git installation does not exist. Using Default
+The recommended git tool is: NONE
+using credential SRVAMR-COMMGIT
+ > git rev-parse --resolve-git-dir /var/lib/jenkins/workspace/Sandbox_Replication-test_main@libs/a1af04574c6d152cee77ec28eba24fcb3b3e2e33ec82f67b79a79b55db45612e/.git # timeout=10
+Fetching changes from the remote Git repository
+ > git config remote.origin.url https://github
