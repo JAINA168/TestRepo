8c8c485d78e3f12656b5693a59e3cea293c2fa63
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-21 09:58:38 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,7 @@
-Started by user srvamr-sfaops
-09:20:48 Connecting to https://api.github.com using SRVAMR-COMMGIT/****** (SRVAMR-COMMGIT)
-Obtained Jenkinsfile from 2956f52cf95fdddc625519d94e192e5b52f02d86
-Loading library sfdi-devops-tools-infra@feature/FIE-228
-Examining pfizer/sfdi-devops-tools-infra
-Attempting to resolve feature/FIE-228 as a branch
-Resolved feature/FIE-228 as branch feature/FIE-228 at revision 90158784d292f795fa7bb713c213ecd1b7797bd8
-Selected Git installation does not exist. Using Default
-The recommended git tool is: NONE
-using credential SRVAMR-COMMGIT
- > git rev-parse --resolve-git-dir /var/lib/jenkins/workspace/Sandbox_Replication-test_main@libs/a1af04574c6d152cee77ec28eba24fcb3b3e2e33ec82f67b79a79b55db45612e/.git # timeout=10
-Fetching changes from the remote Git repository
- > git config remote.origin.url https://github
+<table border="1" style="border-collapse: collapse;">
+    <tr style="background-color: #007bff; color: #ffffff;">
+        <th style="padding: 8px;">Parameter Name</th>
+        <th style="padding: 8px;">Value</th>
+    </tr>
+    ${getParametersTable()}
+</table>
