20196fb5d6302daa53e6fb72de633beb7693fb26
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-27 18:29:44 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,57 +1,26 @@
- <html>
-            <body style="font-family: Arial, sans-serif; background-color: #fff; padding: 20px;">
-              <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-                <h3 style="color: #007bff;">The Jenkins Deployment Job $env.JOB_NAME is $currentBuild.result</h3>
-                <h3>Pod Name: $pod_name</h3>
-                <h3>Job Parameters:</h3>
-                <table border="1" cellpadding="5" cellspacing="0" style="background-color: #e6f3fa; border-collapse: collapse; max-width: 600px; width: 50%;">
-                    <tr>
-                        <th style="width: 150px;">Parameter Name</th>
-                        <th>Value</th>
-                    </tr>
-                    ${getParametersTable()}
-                </table>
-                <hr/>
-                
-                <h3>Build Information:</h3>
-                <table border="1" cellpadding="5" cellspacing="0" style="background-color: #e6f3fa; border-collapse: collapse; max-width: 600px; width: 100%;">
-                    <tr>
-                        <th>Information</th>
-                        <th>Value</th>
-                    </tr>
-                    <tr>
-                        <td>Build URL</td>
-                        <td><a href="$env.BUILD_URL">$env.BUILD_URL</a></td>
-                    </tr>
-                    <tr>
-                        <td>Build Result</td>
-                        <td>$currentBuild.result</td>
-                    </tr>
-                    <tr>
-                        <td>Github URL</td>
-                        <td><a href="$env.GIT_URL">$env.GIT_URL</a></td>
-                    </tr>
-                    <tr>
-                        <td>Github Branch</td>
-                        <td>$env.BRANCH_NAME</td>
-                    </tr>
-                    <tr>
-                        <td>Deployment Request Id</td>
-                        <td>$requestId</td>
-                    </tr>
-                </table>
-                
-                <hr/>
-                
-                
-                <p>
-                    For further queries, please reach out to <a href='mailto:DL-SFADevOps@pfizer.com'>DL-SFADevOps@pfizer.com</a>.
-                </p>
-                
-                <p>
-                    Thanks,<br/>
-                    SFA DevOps Team
-                </p>
-              </div>  
-            </body>
-            </html>
+<table class="MsoNormalTable" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100.0%;border-collapse:collapse">
+        <tbody>
+          <tr>
+            <td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
+              <p class="MsoNormal">
+                <span style="color:black">
+                  <img border="0" width="227" height="65" style="width:2.3645in;height:.677in" id="Picture_x0020_1" src="https://id.pfizer.com/sites/default/files/images/Logo_1.png" alt="Pfizer Logo">
+                </span>
+                <o:p></o:p>
+              </p>
+            </td>
+            <td width="46%" style="width:46.0%;background:white;padding:0in 3.0pt 0=in 4.5pt">
+              <p class="MsoNormal" align="center" style="text-align:center">
+                <b>
+                  <span style="font-size:14.0pt;font-family:'Helvetica',sans-serif;color:black">FOR YOUR AWARENESS </span>
+                </b>
+                <b>
+                  <span style="font-size:14.0pt;font-family:'Helvetica',sans-serif">
+                    <o:p></o:p>
+                  </span>
+                </b>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
