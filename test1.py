ac36033e8458bddb51798067c9b25f605f498e3f
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-28 20:13:49 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,94 +1,12 @@
-          <tr style=3D"height:3.0pt">
-            <td style=3D"padding:0in 0in 0in 0in;height:3.0pt"></td>
-            <td style=3D"padding:0in 0in 0in 0in;height:3.0pt"></td>
-            <td style=3D"padding:0in 0in 0in 0in;height:3.0pt"></td>
-          </tr>
-          <tr style=3D"height:8.0pt">
-            <td style=3D"background:white;padding:0in 0in 0in 0in;height:8.0pt"></td>
-            <td style=3D"background:white;padding:0in 0in 0in 0in;height:8.0pt"></td>
-            <td style=3D"background:white;padding:0in 0in 0in 0in;height:8.0pt"></td>
-          </tr>
-          <tr>
-            <td style=3D"background:#002596;padding:6.0pt 6.0pt 6.0pt 6.0pt">
+            <td width=3D"46%" style=3D"width:46.0%;background:white;padding:0in 3.0pt 0=in 4.5pt">
               <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <span s=tyle=3D"color:white">
-                  <img border=3D"0" width=3D"32" height=3D"28" style=3D"=width:.3333in;height:.2916in" id=3D"Picture_x0020_2" src=3D"cid:what_fya.pn=g">
-                </span>
-                <o:p></o:p>
-              </p>
-            </td>
-            <td style=3D"background:#002596;padding:6.0pt 6.0pt 6.0pt 6.0pt">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <span s=tyle=3D"color:white">
-                  <img border=3D"0" width=3D"51" height=3D"30" style=3D"=width:.5312in;height:.3125in" id=3D"Picture_x0020_3" src=3D"cid:who.png">
-                  </= span>
-                  <o:p></o:p>
-              </p>
-            </td>
-            <td style=3D"background:#002596;padding:6.0pt 6.0pt 6.0pt 6.0pt">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <span s=tyle=3D"color:white">
-                  <img border=3D"0" width=3D"28" height=3D"32" style=3D"=width:.2916in;height:.3333in" id=3D"Picture_x0020_4" src=3D"cid:when.png">
-                  <= /span>
+                <b>
+                  <spa= n style=3D"font-size:14.0pt;font-family:&quot;Helvetica&quot;,sans-serif;color:black">FOR YOUR AWARENESS </span>
+                </b>
+                <b>
+                  <span style=3D"font-size:14.0pt;=font-family:&quot;Helvetica&quot;,sans-serif">
                     <o:p></o:p>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td style=3D"background:#002596;padding:0in 0in 6.0pt 0in">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <b>
-                  <spa= n style=3D"font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=white">What <o:p></o:p>
-                    </span>
+                  </span>
                 </b>
               </p>
             </td>
-            <td style=3D"background:#002596;padding:0in 0in 6.0pt 0in">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <b>
-                  <spa= n style=3D"font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=white">Who <o:p></o:p>
-                    </span>
-                </b>
-              </p>
-            </td>
-            <td style=3D"background:#002596;padding:0in 0in 6.0pt 0in">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <b>
-                  <spa= n style=3D"font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=white">When <o:p></o:p>
-                    </span>
-                </b>
-              </p>
-            </td>
-          </tr>
-          <tr>
-            <td style=3D"padding:6.0pt 0in 6.0pt 0in">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <b>
-                  <spa= n style=3D"font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=#002596">FOR YOUR AWARENESS:Confluence | SSO Service Upgrade <o:p></o:p>
-                    </sp= an>
-                </b>
-              </p>
-            </td>
-            <td style=3D"padding:0in 0in 0in 0in">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <b>
-                  <spa= n style=3D"font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=#002596">All Confluence Consumers <o:p></o:p>
-                    </span>
-                </b>
-              </p>
-            </td>
-            <td style=3D"padding:0in 0in 0in 0in">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
-                <b>
-                  <spa= n style=3D"font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=#002596">Friday September 22 6.00 PM - Sunday September 24 8.00 PM ET <o:p>
-                      <= /o:p>
-                        </span>
-                </b>
-              </p>
-            </td>
-          </tr>
-          <tr style=3D"height:6.0pt">
-            <td style=3D"background:#002596;padding:0in 0in 0in 0in;height:6.0pt"></td>
-            <td style=3D"background:#002596;padding:0in 0in 0in 0in;height:6.0pt"></td>
-            <td style=3D"background:#002596;padding:0in 0in 0in 0in;height:6.0pt"></td>
-          </tr>
