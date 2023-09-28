0b7ece2760b885b16800923f5e04b3dd724784a0
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-28 12:46:51 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,37 @@
-<td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
-              <p class="MsoNormal">
-                <span style="color:black; text-align: right;">
-                  <img border="0" width="100" height="50" style="max-width:100%; height:auto;" id="Picture_x0020_2" src="https://example.com/your-image-2.png" alt="Image 2">
-                </span>
-                <o:p></o:p>
-              </p>
-            </td>
+            <html>
+            <body style="font-family: Arial, sans-serif; background-color: #fff; padding: 20px;">
+              <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+                <table class="MsoNormalTable" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100.0%;border-collapse:collapse">
+					<tbody>
+					<tr>
+						<td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
+						<p class="MsoNormal">
+							<span style="color:black">
+							<img border="0" width="150" height="65" style="max-width:100%; height:auto;" id="Picture_x0020_1"src="https://team.design/wp-content/uploads/2022/08/03_Pfizer_Logo.png" alt="Pfizer Logo">
+							</span>
+							<o:p></o:p>
+						</p>
+						</td>
+						<td width="46%" style="width:46.0%;background:white;padding:0in 3.0pt 0=in 4.5pt">
+						  
+							<b>
+							<span style="font-size:14.0pt;font-family:'Helvetica',sans-serif;color:black">Jenkins Deployment </span>
+							</b>
+							<b>
+							<span style="font-size:14.0pt;font-family:'Helvetica',sans-serif">
+								<o:p></o:p>
+							</span>
+							</b>
+						  
+						</td>
+             					<td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
+              						<p class="MsoNormal">
+                						<span style="color:black;">
+                  							<img border="0" width="100" height="50" style="max-width:100%; height:auto;" id="Picture_x0020_2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Jenkins_logo_with_title.svg/1280px-Jenkins_logo_with_title.svg.png" alt="Jenkins Logo">
+               							 </span>
+                						<o:p></o:p>
+              						</p>
+            					</td>
+					</tr>
+					</tbody>
+				</table>
