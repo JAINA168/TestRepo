a78f61cf441826e2e41746173532fb1fbde448c3
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-28 12:31:25 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,26 +1,8 @@
-<table class="MsoNormalTable" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100.0%;border-collapse:collapse">
-        <tbody>
-          <tr>
-            <td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
+<td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
               <p class="MsoNormal">
-                <span style="color:black">
-                  <img border="0" width="227" height="65" style="width:2.3645in;height:.677in" id="Picture_x0020_1" src="https://id.pfizer.com/sites/default/files/images/Logo_1.png" alt="Pfizer Logo">
+                <span style="color:black; text-align: right;">
+                  <img border="0" width="100" height="50" style="max-width:100%; height:auto;" id="Picture_x0020_2" src="https://example.com/your-image-2.png" alt="Image 2">
                 </span>
                 <o:p></o:p>
               </p>
             </td>
-            <td width="46%" style="width:46.0%;background:white;padding:0in 3.0pt 0=in 4.5pt">
-              <p class="MsoNormal" align="center" style="text-align:center">
-                <b>
-                  <span style="font-size:14.0pt;font-family:'Helvetica',sans-serif;color:black">FOR YOUR AWARENESS </span>
-                </b>
-                <b>
-                  <span style="font-size:14.0pt;font-family:'Helvetica',sans-serif">
-                    <o:p></o:p>
-                  </span>
-                </b>
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
