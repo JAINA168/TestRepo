7899dd5d695d9fb33f1d953a169aedc189d497f9
HemapriyaM23 <127109144+HemapriyaM23@users.noreply.github.com>
2023-09-29 18:52:32 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,183 @@
-            <td width=3D"46%" style=3D"width:46.0%;background:white;padding:0in 3.0pt 0=in 4.5pt">
-              <p class=3D"MsoNormal" align=3D"center" style=3D"text-align:center">
+<html>
+  <body style="font-family: Arial, sans-serif; background-color: #fff; padding: 20px;">
+    <div style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+      <table class="MsoNormalTable" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100.0%;border-collapse:collapse">
+        <tbody>
+          <tr>
+            <td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
+              <p class="MsoNormal">
+                <span style="color:black">
+                <img border="0" width="150" height="65" style="max-width:100%; height:auto;" id="Picture_x0020_1"src="https://team.design/wp-content/uploads/2022/08/03_Pfizer_Logo.png" alt="Pfizer Logo">
+                </span>
+                <o:p></o:p>
+              </p>
+            </td>
+            <td width="46%" style="width:46.0%;background:white;padding:0in 3.0pt 0=in 4.5pt">
+              <p class="MsoNormal" align="center" style="text-align:center">
                 <b>
-                  <spa= n style=3D"font-size:14.0pt;font-family:&quot;Helvetica&quot;,sans-serif;color:black">FOR YOUR AWARENESS </span>
+                  <span style="font-size:14.0pt;font-family:&quot;Helvetica&quot;,sans-serif;color:black">SFA-JENKINS DEPLOYMENT AWARENESS</span>
                 </b>
                 <b>
-                  <span style=3D"font-size:14.0pt;=font-family:&quot;Helvetica&quot;,sans-serif">
+                  <span style="font-size:14.0pt;=font-family:&quot;Helvetica&quot;,sans-serif">
                     <o:p></o:p>
                   </span>
                 </b>
               </p>
             </td>
+            <td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in">
+              <p class="MsoNormal" align="right" style="text-align:right">
+                <span style="color:black">
+                <img border="0" width="100" height="45" style="max-width:100%; height:auto;" id="Picture_x0020_2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Jenkins_logo_with_title.svg/1280px-Jenkins_logo_with_title.svg.png" alt="Jenkins Logo">
+                </span>
+                <o:p></o:p>
+              </p>
+            </td>
+            <td width="27%" style="width:27.0%;background:white;padding:0in 0in 0in=0in"></td>
+          </tr>
+          <tr style="height:3.0pt">
+            <td style="padding:0in 0in 0in 0in;height:3.0pt"></td>
+            <td style="padding:0in 0in 0in 0in;height:3.0pt"></td>
+            <td style="padding:0in 0in 0in 0in;height:3.0pt"></td>
+          </tr>
+          <tr style="height:8.0pt">
+            <td style="background:white;padding:0in 0in 0in 0in;height:8.0pt"></td>
+            <td style="background:white;padding:0in 0in 0in 0in;height:8.0pt"></td>
+            <td style="background:white;padding:0in 0in 0in 0in;height:8.0pt"></td>
+          </tr>
+          <tr>
+            <td style="background:#002596;padding:6.0pt 6.0pt 6.0pt 6.0pt">
+              <p class="MsoNormal" align="center" style="text-align:center">
+                <span style="color:white">
+                <img border="0" width="52" height="38" style="max-width:.3333in;height:.2916in" id="Picture_x0020_3" src="https://www.freepnglogos.com/uploads/email-png/email-western-libraries-12.png">
+                </span>
+                <o:p></o:p>
+              </p>
+            </td>
+            <td style="background:#002596;padding:6.0pt 6.0pt 6.0pt 6.0pt">
+              <p class="MsoNormal" align="center" style="text-align:center">
+                <span s=tyle="color:white">
+                  <img border="0" width="30" height="38" style="max-width:.3333in;height:.2916in" id="Picture_x0020_3" src="https://cdn-icons-png.flaticon.com/512/4500/4500935.png">
+                  </= span>
+                  <o:p></o:p>
+              </p>
+            </td>
+            <td style="background:#002596;padding:6.0pt 6.0pt 6.0pt 6.0pt">
+            <p class="MsoNormal" align="center" style="text-align:center">
+            <span s=tyle="color:white">
+            <img border="0" width="40" height="48" style="max-width:.3333in;height:.2916in" id="Picture_x0020_4" src="https://www.freeiconspng.com/uploads/calender-icon-blue-18.png">
+            </span>
+            <o:p></o:p>
+            </p>
+            </td>
+          </tr>
+          <tr>
+          <td style="background:#002596;padding:0in 0in 6.0pt 0in">
+          <p class="MsoNormal" align="center" style="text-align:center">
+          <b>
+          <spa= n style="font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=white">What <o:p></o:p>
+          </span>
+          </b>
+          </p>
+          </td>
+          <td style="background:#002596;padding:0in 0in 6.0pt 0in">
+          <p class="MsoNormal" align="center" style="text-align:center">
+          <b>
+          <spa= n style="font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=white">Where <o:p></o:p>
+          </span>
+          </b>
+          </p>
+          </td>
+          <td style="background:#002596;padding:0in 0in 6.0pt 0in">
+          <p class="MsoNormal" align="center" style="text-align:center">
+          <b>
+          <spa= n style="font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=white">When <o:p></o:p>
+          </span>
+          </b>
+          </p>
+          </td>
+          </tr>
+          <tr>
+            <td style="padding:6.0pt 0in 6.0pt 0in">
+              <p class="MsoNormal" align="center" style="text-align:center">
+                <b>
+                  <spa= n style="font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=#002596">Jenkins deployment for '$pod_name' is $currentBuild.result 
+                  <o:p></o:p>
+                  </sp= an>
+                </b>
+              </p>
+            </td>
+            <td style="padding:0in 0in 0in 0in">
+              <p class="MsoNormal" align="center" style="text-align:center">
+                <b>
+                  <spa= n style="font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=#002596">$convertedEnv Environment
+                  <o:p></o:p>
+                  </span>
+                </b>
+              </p>
+            </td>
+            <td style="padding:0in 0in 0in 0in">
+              <p class="MsoNormal" align="center" style="text-align:center">
+                <b>
+                  <spa= n style="font-size:12.0pt;font-family:&quot;Arial&quot;,sans-serif;color:=#002596">$env.BUILD_TIMESTAMP
+                  <o:p></o:p>
+                  </span>
+                </b>
+              </p>
+            </td>
+          </tr>
+          <tr style="height:6.0pt">
+            <td style="background:#002596;padding:0in 0in 0in 0in;height:6.0pt"></td>
+            <td style="background:#002596;padding:0in 0in 0in 0in;height:6.0pt"></td>
+            <td style="background:#002596;padding:0in 0in 0in 0in;height:6.0pt"></td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 style="color: #007bff;">The Jenkins Deployment Job $env.JOB_NAME is $currentBuild.result</h3>
+      <h3>Pod Name: $pod_name</h3>
+      <h3>Job Parameters:</h3>
+      <table border="1" cellpadding="5" cellspacing="0" style="background-color: #e6f3fa; border-collapse: collapse; max-width: 600px; width: 50%;">
+        <tr>
+          <th style="width: 150px;">Parameter Name</th>
+          <th>Value</th>
+        </tr>
+        ${getParametersTable()}
+      </table>
+      <hr/>
+      <h3>Build Information:</h3>
+      <table border="1" cellpadding="5" cellspacing="0" style="background-color: #e6f3fa; border-collapse: collapse; max-width: 600px; width: 100%;">
+        <tr>
+          <th>Information</th>
+          <th>Value</th>
+        </tr>
+        <tr>
+          <td>Build URL</td>
+          <td><a href="$env.BUILD_URL">$env.BUILD_URL</a></td>
+        </tr>
+        <tr>
+          <td>Build Result</td>
+          <td>$currentBuild.result</td>
+        </tr>
+        <tr>
+          <td>Github URL</td>
+          <td><a href="$env.GIT_URL">$env.GIT_URL</a></td>
+        </tr>
+        <tr>
+          <td>Github Branch</td>
+          <td>$env.BRANCH_NAME</td>
+        </tr>
+        <tr>
+          <td>Deployment Request Id</td>
+          <td>$requestId</td>
+        </tr>
+      </table>
+      <hr/>
+      <p>
+        For further queries, please reach out to <a href='mailto:hemapriya.m@pfizer.com'>hemapriya.m@pfizer.com</a>.
+      </p>
+      <p>
+        Thanks,<br/>
+        SFA DevOps Team
+      </p>
+    </div>
+  </body>
+</html>
